79228853a67de19d1e6c2a31de2f33147a40026d
rohan <rohan@claclaws.com>
2024-07-08 16:58:26 -0400

    Added bottom scroll get history data or messages for a particular pdf for a particular userid in order

--- src/app/api/getMessages/route.ts
+++ src/app/api/getMessages/route.ts
@@ -1,7 +1,17 @@
 import { NextResponse } from "next/server";
-
+import {db} from "@/lib/db/index"
+import {messages as messageTable} from "@/lib/db/schema"
+import { eq } from "drizzle-orm";
 export async function POST(request:Request,response:Response){
-    console.log("Request Body ",await request.json())
-    return NextResponse.json("hello post method")
+    try{
+
+        const {chatid}= await request.json();
+        const queryResult=await db.select().from(messageTable).where(eq(messageTable.chatId,chatid)).orderBy(messageTable.createdAt);
+        return NextResponse.json({status:'success',messagesforPdf:queryResult});
+        
+    }catch(err){
+        console.log("Error in Getting messages",err);
+        throw err; 
+    }
 
 }
--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Input } from '@/components/ui/input'
 import { Button } from './ui/button'
 import { useChat } from "ai/react"
@@ -8,47 +8,64 @@ import { RiSendPlaneFill } from "react-icons/ri";
 import { cn } from '@/lib/utils'
 import toast from 'react-hot-toast'
 import { useQuery } from '@tanstack/react-query'
-
+import { useSearchParams, usePathname } from 'next/navigation'
+import { Message } from 'ai'
 
 type Props = {
   activeId?: number
 }
 
-
-const callToMessagesApi=async(chatid:number)=>{
-  try{
-    const apiresult=await fetch('http://localhost:3000/api/getMessages',{
-      method:'POST',
-      body:JSON.stringify({chatid:chatid}),
+const callToMessagesApi = async (chatid: number) => {
+  try {
+    const apiresult = await fetch('http://localhost:3000/api/getMessages', {
+      method: 'POST',
+      body: JSON.stringify({ chatid: chatid }),
     })
-    await apiresult.json();
-  }catch(err){
-    console.log("Error in Calling Get Message API ",err);
+    const apiResultMessages = await apiresult.json()
+    console.log("Messages", apiResultMessages);
+    return apiResultMessages;
+  } catch (err) {
+    console.log("Error in Calling Get Message API ", err);
     throw err;
   }
 }
 
 const ChatComponent = ({ activeId }: Props) => {
 
-    useQuery({
-      queryKey:["chat",activeId],
-      queryFn:async ()=>{await callToMessagesApi(activeId as number)},
-      
+  const pathname = usePathname()
+  const searchparams = useSearchParams();
 
-    })
+  //* Function to get Messages stored in db 
+  const { data, isLoading } = useQuery({
+    queryKey: ["chat", activeId],
+    queryFn: async () => {
+      const previousMessages = await callToMessagesApi(activeId as number)
+      return previousMessages?.messagesforPdf
+    },
+  })
+
+  console.log("Data", data);
 
   const { input, handleInputChange, handleSubmit, messages } = useChat({
     api: '/api/chat',
     body: {
       chatid: activeId
     },
-
-
+    initialMessages: data || [],
     onError: (err) => {
       toast.error("Something went wrong ! Try later", { id: "error-chat" });
     }
   });
 
+  useEffect(() => {
+    const messageContainer = document.getElementById("message-container");
+    if (messageContainer) {
+      messageContainer.scrollTo({
+        top: messageContainer.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [messages]);
 
   return (
     <div className='relative  overflow-y-hidden h-full max-h-full'>
@@ -62,20 +79,26 @@ const ChatComponent = ({ activeId }: Props) => {
       {
         messages.length > 0 &&
         <div
-          className={cn("max-h-full overflow-y-auto ", {
-            "pb-[90px]": messages.length > 0
+          id={'message-container'}
+          className={cn("max-h-full overflow-y-auto  relative", {
+            "pb-[90px]": messages.length > 0,
+            "relative": ""
           })}
         >
-          <MessageList messages={messages} />
+          <MessageList messages={messages} isLoading={isLoading} />
         </div>
       }
-
-      <form className='flex w-full space-x-2 sticky bottom-[0px] md:bottom-[-1px] inset-x-0 px-2 py-3  bg-white z-30 ' onSubmit={handleSubmit} >
-        <Input type="text" value={input} onChange={handleInputChange} placeholder="Ask anything about the PDF" className='w-full' />
-        <Button type="submit" className='flex items-center gap-x-2'>
-          <p className=''>Send</p>
-          <RiSendPlaneFill className='self-center text-md ' /></Button>
-      </form>
+      {
+        pathname === '/chat' && searchparams.size == 0 ?
+          <h1 className=' ml-2 text-lg font-medium'>Nothing to chat</h1>
+          :
+          <form className='flex w-full space-x-2 sticky bottom-[0px] md:bottom-[-1px] inset-x-0 px-2 py-3  bg-white z-30 ' onSubmit={handleSubmit} >
+            <Input type="text" value={input} onChange={handleInputChange} placeholder="Ask anything about the PDF" className='w-full' />
+            <Button type="submit" className='flex items-center gap-x-2'>
+              <p className=''>Send</p>
+              <RiSendPlaneFill className='self-center text-md ' /></Button>
+          </form>
+      }
 
     </div>
   )
