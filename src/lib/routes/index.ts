8cdc87b20e47c1ffdb0fdc3858012d8006f28691
Rohan Vania <70309298+RohanVania@users.noreply.github.com>
2024-07-11 16:54:05 -0400

    Update index.ts

--- src/lib/routes/index.ts
+++ src/lib/routes/index.ts
@@ -4,7 +4,14 @@
 //     getpdfData:"http://localhost:3000/api/pdfdata",
 // }
 
+// export const Routes={
+//     createchat:"http://localhost:3000/api/create-chat",
+//     getpdfData:"http://localhost:3000/api/pdfdata",
+// }
+
+// https://aichat-pdf.vercel.app
+
 export const Routes={
-    createchat:"http://localhost:3000/api/create-chat",
-    getpdfData:"http://localhost:3000/api/pdfdata",
+    createchat:"https://aichat-pdf.vercel.app/api/create-chat",
+    getpdfData:"https://aichat-pdf.vercel.app/api/pdfdata",
 }
